12f3acd96d899ffd1c355ef727a3a2d5c0acbe18
Akua Adomako <akuaadomako059@gmail.com>
2025-01-29 15:11:38 +0000

    nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 // navigation sidebar responsive
-function toggleMenu() {
-  const menu = document.querySelector('nav ul');
-  const hamburger = document.querySelector('.hamburger');
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 900) {
+    const menu = document.querySelector("nav ul");
+    const hamburger = document.querySelector(".hamburger");
+    const [close1, , close3] = hamburger.querySelectorAll("span");
 
-  menu.classList.toggle('active');
-  hamburger.classList.toggle('open');
-  
-}
+    menu.classList.remove("active");
+    hamburger.classList.remove("open");
+    close1.classList.remove("close");
+    close3.classList.remove("close");
+  }
+});
+
+const toggleMenu = () => {
+  const menu = document.querySelector("nav ul");
+  const hamburger = document.querySelector(".hamburger");
+  const [close1, , close3] = hamburger.querySelectorAll("span");
+
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("open");
+  close1.classList.toggle("close");
+  close3.classList.toggle("close");
+};
